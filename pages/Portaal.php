372d5d16739b8156d71d879201c1e96229fcd512
aishava <aisha.ambare@edu-kw1c.nl>
2023-09-28 11:34:01 +0200

    portaal pagina

--- pages/Portaal.php
+++ pages/Portaal.php
@@ -18,14 +18,15 @@
                     <img src="<?= ROOT ?>images/sterren2.gif" style="position: absolute;">
             </div>
         </a>
-        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center text-kleur" >
+        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center text-kleur">
         <!--Buttons met informatie-->
 
         <p>
-                <img src="<?= ROOT ?>images/duurzaamheid_icoon.png" class="heartbeat-img" style="position: absolute; margin-top: 40%; margin-left: 45%;" data-toggle="collapse" data-target="#duurzaam">
-                <div class="b-background3 collapse show" style="position: absolute; margin-top: 55%; margin-right: 50%;" id="duurzaam">
+                <img src="<?= ROOT ?>images/duurzaamheid_icoon.png" class="heartbeat-img groen " style="position: absolute; margin-top: 40%; margin-left: 45%;" data-toggle="collapse" data-target="#duurzaam">
+
+                <div class="tekstvakken b-background3 collapse show tekstvakgroen" style="position: absolute; margin-top: 55%; margin-right: 50%;" id="duurzaam">
                         <h1>Duurzaamheid</h1>
-                        <p class="txt-button bb-background3" style="font-size: 28px;">
+                        <p class="txt-button bb-background3 backgroundfont-class " style="font-size: 28px;">
                         Duurzaamheid is van belang voor de toekomst van onze planeet en de mensheid. <br>
                         Hoe ziet jouw wereld eruit over 25 jaar? <br>
                         Zijn er dan nog olifanten? Reizen we nog in vliegtuigen?
@@ -33,22 +34,23 @@
                 </div>
         </p>
         <p>
-                <img src="<?= ROOT ?>images/vrede_en_mensenrechten_icoon.png" class="heartbeat-img" style="position: absolute; margin-top: 95%; margin-left: 45%;" data-toggle="collapse" data-target="#vrede">
-                <div class="b-background4 collapse show" style="position: absolute; margin-top: 110%; margin-left: 55%;" id="vrede">
-                        <h1>Vrede en mensenrechten</h1>
-                        <p class="txt-button bb-background4" style="font-size: 28px;">
-                        Mensenrechten gelden voor iedereen, ongeacht waar je geboren bent of wie je bent. <br> 
-                        Vrede is afwezigheid van geweld en oorlog. <br> 
-                        Hoe kunnen we zorgen dat mensenrechten beschermd worden? <br> 
-                        Is het mogelijk om wereldvrede te bereiken? En wat is jouw rol daarin?
-                        </p>                           
+                <img src="<?= ROOT ?>images/vrede_en_mensenrechten_icoon.png" class="heartbeat-img geel" style="position: absolute; margin-top: 95%; margin-left: 45%;" data-toggle="collapse" data-target="#vrede">
+                <div class="tekstvakken b-background4 collapse show  tekstvakgeel" style="position: absolute; margin-top: 110%; margin-left: 55%;" id="vrede">
+                    <h1>Vrede en mensenrechten</h1>
+                            <p class="txt-button bb-background4 backgroundfont-class tekstvakgeel" style="font-size: 28px;">
+
+                                Mensenrechten gelden voor iedereen, ongeacht waar je geboren bent of wie je bent. <br>
+                                Vrede is afwezigheid van geweld en oorlog. <br>
+                                Hoe kunnen we zorgen dat mensenrechten beschermd worden? <br>
+                                Is het mogelijk om wereldvrede te bereiken? En wat is jouw rol daarin?
+                            </p>
                 </div>
         </p>
         <p>
-                <img src="<?= ROOT ?>images/intercultureel_leren.png"class="heartbeat-img" style="position: absolute; margin-top: 165%; margin-left: 45%;" data-toggle="collapse" data-target="#cultuur">
-                <div class="b-background5 collapse show" style="position: absolute; margin-top: 180%; margin-right: 50%;" id="cultuur">
+                <img src="<?= ROOT ?>images/intercultureel_leren.png"class="heartbeat-img roze" style="position: absolute; margin-top: 165%; margin-left: 45%;" data-toggle="collapse" data-target="#cultuur">
+                <div class="tekstvakken b-background5 collapse show tekstvakroze" style="position: absolute; margin-top: 180%; margin-right: 50%;" id="cultuur">
                         <h1>Intercultureel</h1>
-                        <p class="txt-button bb-background5" style="font-size: 28px;">
+                        <p class="txt-button bb-background5 backgroundfont-class tekstvakroze " style="font-size: 28px;">
                         Intercultureel leren betekent het ontwikkelen van kennis over andere culturen en andere manieren van leven. <br>
                         Als je openstaat voor andere culturen gaat er een wereld voor je open. <br> 
                         Wat wil jij leren van andere culturen?
@@ -56,10 +58,10 @@
                 </div>
         </p>
         <p>
-                <img src="<?= ROOT ?>images/wereldburgerschap2.png" class="heartbeat-img" style="position: absolute; margin-top: 270%; margin-left: 45%;" data-toggle="collapse" data-target="#wereld">
-                <div class="b-background6 collapse show" style="position: absolute; margin-top: 285%; margin-left: 50%;" id="wereld">
+                <img src="<?= ROOT ?>images/wereldburgerschap_icoon.png" class="heartbeat-img blauw" style="position: absolute; margin-top: 270%; margin-left: 45%;" data-toggle="collapse" data-target="#wereld">
+                <div class="tekstvakken b-background6 collapse show tekstblauw" style="position: absolute; margin-top: 285%; margin-left: 50%;" id="wereld">
                         <h1>Wereldburgerschap</h1>
-                        <p class="txt-button bb-background6" style="font-size: 28px;">
+                        <p class="txt-button bb-background6 backgroundfont-class tekstblauw" style="font-size: 28px;">
                         Als wereldburgers zijn we samen verantwoordelijk voor elkaar en de wereld. <br>
                         We gaan voor een betere wereld. <br>
                         Hoe werk je samen met mensen die anders denken dan jij? <br>
@@ -70,7 +72,7 @@
                 <button style="position: absolute; margin-top: 400%; font-size: 300%; " class="btn btn-quiz" onclick="window.location.href = 'Quiz.php';">Ontdek je Innerlijke Held</button>
                 <img src="<?= ROOT ?>images/heldenlijn.png" class="heldenlijn-class img-fluid" style="margin-top: 4%;">            
         </div>
-        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center" style="margin-top: 131%;">
+        <div class="col-12 col-lg-6 mx-5 d-flex justify-content-center leegte" style="margin-top: 131%;">
                 <img src="<?= ROOT ?>images/eindlijn.png" class="heldenlijn-class img-fluid">   
         </div>
         <div class="col-12 mx-5 d-flex justify-content-center" style="margin-top: 85%;">
